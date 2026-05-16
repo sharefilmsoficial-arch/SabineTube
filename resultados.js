cd0aa070fa4935d89a7c259531e8291c846a0174
ShareFilmsOficial <sharefilmsoficial@gmail.com>
2026-05-15 19:17:26 -0600

    Update resultados.js

--- resultados.js
+++ resultados.js
@@ -2,6 +2,14 @@ const grid = document.getElementById("moviesGrid");
 const title = document.getElementById("resultsTitle");
 const searchInput = document.getElementById("search");
 
+
+// función para quitar acentos
+function normalize(text) {
+  return text
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+}
 // leer parámetro ?q=
 const params = new URLSearchParams(window.location.search);
 const query = params.get("query")?.toLowerCase() || "";
@@ -9,9 +17,20 @@ const query = params.get("query")?.toLowerCase() || "";
 searchInput.value = query;
 
 // filtrar películas
-const results = MOVIES.filter(movie =>
-  movie.title.toLowerCase().includes(query)
-);
+const results = MOVIES.filter(movie => {
+
+  // buscar en título
+  const titleMatch =
+    normalize(movie.title).includes(normalize(query));
+
+  // buscar en keywords
+  const keywordMatch =
+    movie.keywords?.some(keyword =>
+      normalize(keyword).includes(normalize(query))
+    );
+
+  return titleMatch || keywordMatch;
+});
 
 // título tipo YouTube
 title.textContent = results.length
